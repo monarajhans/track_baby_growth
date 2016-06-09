81e4de1b879c794f6f32f39b6c4c82f6dc1de6c6
Mona Rajhans <mona@glooko.com>
2016-06-08 17:14:25 -0700

    Creating the table for baby's favorite animal

--- track_baby_growth/db/schema.rb
+++ track_baby_growth/db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160608231817) do
+ActiveRecord::Schema.define(version: 20160609001204) do
 
   create_table "babies", force: :cascade do |t|
     t.string   "name"
     t.integer  "age"
     t.integer  "height"
     t.integer  "weight"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorite_animals", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "height"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
