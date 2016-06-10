56d51ef68678beaf17c3c1d7b3593d2f3e5fbcd2
Mona Rajhans <mona@glooko.com>
2016-06-10 10:56:31 -0700

    Adding logc for baby's favorite animal

--- track_baby_growth/app/models/baby.rb
+++ track_baby_growth/app/models/baby.rb
@@ -3,12 +3,4 @@ class Baby < ActiveRecord::Base
 	validates :height, presence: true, numericality: {greater_than_or_equal_to: 1}
 	validates :weight, presence: true, numericality: {greater_than_or_equal_to: 1}
 	validates :age, presence: true, numericality: {greater_than_or_equal_to: 1}
-
-	# def self.favorite_animal
-	#     create_table :favorite_animals do |t|
-	#       t.string :name
-	#       t.integer :height
-	#       t.timestamps
-	#     end
-	# end
 end
